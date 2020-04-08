37bad4bcb5bb3fa4c7ef8db2e9a1f90a29668b99
Crystal Broach <crysland@gmail.com>
2020-04-07 19:34:19 -0500

    Created base Clinker Controller and base requests

--- ClinkedIn/Controllers/ClinkerController.cs
+++ ClinkedIn/Controllers/ClinkerController.cs
@@ -11,5 +11,42 @@ namespace ClinkedIn.Controllers
     [ApiController]
     public class ClinkerController : ControllerBase
     {
-    }
+        ClinkerRepo _repository = new ClinkerRepo();
+
+        [HttpPost]
+        public IActionResult AddClinker(Clinker clinkerToAdd)
+        {
+            var existingClinker = _repository.GetByFullName();
+            if (existingClinker == null)
+            {
+                _repository.Add(clinkerToAdd);           
+                return Created("", clinkerToAdd);
+            }
+            else
+            {
+                return BadRequest("User already exists, please try again.");
+            }
+        }
+
+        [HttpGet]
+        public IActionResult GetAllClinkers()
+        {
+            var allClinkers = _repository.GetAll();
+
+            return Ok(allClinkers);
+        }
+
+        [HttpGet("{interest}")]
+        public IActionResult GetClinkerByInterest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut]
+        public IActionResult UpdateClinker()
+        {
+            throw new NotImplementedException();
+        }
+
+}
 }
